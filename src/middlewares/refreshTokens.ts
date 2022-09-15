1ba0ee446ba3d79de9590d83feb548b117466151
Tesseract <tensai3355@gmail.com>
2022-09-15 17:23:24 +0900

    :bug: if문을 통해 분기되어 실행된 미들웨어의 에러가 처리되지 않는 버그 수정

--- src/middlewares/refreshTokens.ts
+++ src/middlewares/refreshTokens.ts
@@ -1,27 +1,37 @@
 import { createAccessToken, createRefreshToken } from '@services/auth';
 import { updateUserByUserId } from '@services/user';
+import handleError, { CustomError } from './handleError';
 
 const refreshTokens: HandlerWithCallback =
   (callback) => async (req, res, next) => {
-    const {
-      refreshToken: { userId }
-    } = req;
-    const newAccessToken = createAccessToken(userId as number);
-    const newRefreshToken = createRefreshToken(userId as number);
-    await updateUserByUserId(userId as number, {
-      refresh_token: newRefreshToken
-    });
-    res.clearCookie('auth');
-    res.cookie('auth', newRefreshToken, {
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-      httpOnly: true,
-      signed: true
-    });
-    req.addon = { ...(req.addon ?? {}), accessToken: newAccessToken };
-    if (callback) {
-      callback(req, res);
-    } else {
-      next();
+    try {
+      const {
+        refreshToken: { userId }
+      } = req;
+      const newAccessToken = createAccessToken(userId as number);
+      const newRefreshToken = createRefreshToken(userId as number);
+      await updateUserByUserId(userId as number, {
+        refresh_token: newRefreshToken
+      });
+      res.clearCookie('auth');
+      res.cookie('auth', newRefreshToken, {
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        signed: true
+      });
+      req.addon = { ...(req.addon ?? {}), accessToken: newAccessToken };
+      if (callback) {
+        callback(req, res);
+      } else {
+        next();
+      }
+    } catch {
+      handleError(
+        new CustomError('009', '토큰 발급 과정에서 에러 발생'),
+        req,
+        res,
+        next
+      );
     }
   };
 
--- src/middlewares/tokenHandlers.ts
+++ src/middlewares/tokenHandlers.ts
@@ -1,23 +1,27 @@
 import { RequestHandler } from 'express';
-import { CustomError } from '@middlewares/handleError';
+import handleError, { CustomError } from '@middlewares/handleError';
 import verifyAccessToken from '@middlewares/verifyAccessToken';
 import verifyRefreshToken from '@middlewares/verifyRefreshToken';
 import refreshTokens from './refreshTokens';
 
 const handleRefreshToken: RequestHandler = async (req, res, next) => {
-  const { refreshToken } = req;
-  if (refreshToken.status === 'NO_TOKEN') {
-    throw new CustomError('001', 'refresh token이 존재하지 않음', {
-      clearAccessToken: true,
-      clearRefreshToken: true
-    });
-  } else if (refreshToken.status === 'INVALID') {
-    throw new CustomError('002', 'refresh token이 유효하지 않음', {
-      clearAccessToken: true,
-      clearRefreshToken: true
-    });
-  } else {
-    refreshTokens()(req, res, next);
+  try {
+    const { refreshToken } = req;
+    if (refreshToken.status === 'NO_TOKEN') {
+      throw new CustomError('001', 'refresh token이 존재하지 않음', {
+        clearAccessToken: true,
+        clearRefreshToken: true
+      });
+    } else if (refreshToken.status === 'INVALID') {
+      throw new CustomError('002', 'refresh token이 유효하지 않음', {
+        clearAccessToken: true,
+        clearRefreshToken: true
+      });
+    } else {
+      refreshTokens()(req, res, next);
+    }
+  } catch (error) {
+    handleError(error, req, res, next);
   }
 };
 
