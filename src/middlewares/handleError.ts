bd9a50f4bb78a27ee75bfbebebd0c1ffc589fd09
Tesseract <tensai3355@gmail.com>
2022-09-15 17:24:33 +0900

    :pencil2: 에러 코드 추가

--- src/middlewares/handleError.ts
+++ src/middlewares/handleError.ts
@@ -32,7 +32,9 @@ const statusMap: Partial<Record<ErrorCode, number>> = {
   '005': 400, // 회원가입 폼 입력 정보가 유효하지 않음
   '006': 404, // 회원가입을 위한 임시 회원 정보(OAuth 로그인시 자동 생성, auth 정보와 임시 발급된 닉네임 정보만 존재)가 존재하지 않음,
   '007': 403,
-  '008': 400 // OAuth 리디렉션 후 전달받은 user 정보가 유효하지 않음
+  '008': 400, // OAuth 리디렉션 후 전달받은 user 정보가 유효하지 않음,
+  '009': 500, // 토큰 발급 과정에서 에러 발생
+  '010': 401 // Referer가 올바르지 않음
 };
 
 const handleError: ErrorRequestHandler = (error, req, res, next) => {
