a5cf5f4f9310e75da143b9556311cba1ad5ccbc3
Tesseract <tensai3355@gmail.com>
2022-09-17 03:07:15 +0900

    :bug: OAuth 콜백 요청시 referer 검증을 하지 않도록 수정

--- src/middlewares/verifyReferer.ts
+++ src/middlewares/verifyReferer.ts
@@ -7,8 +7,14 @@ dotenv.config();
 const { DOMAIN } = process.env;
 
 const verifyReferer: RequestHandler = async (req, res, next) => {
-  const { referer } = req.headers;
-  if (typeof referer !== 'string' || !referer.startsWith(DOMAIN)) {
+  const {
+    originalUrl,
+    headers: { referer }
+  } = req;
+  if (
+    !originalUrl.startsWith('/auth') &&
+    (typeof referer !== 'string' || !referer.startsWith(DOMAIN))
+  ) {
     throw new CustomError('010', '잘못된 Referer');
   } else {
     next();
