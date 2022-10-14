da7dd49d4359318b92c1c1ca27cb1de9789e4554
Tesseract <tensai3355@gmail.com>
2022-10-15 02:01:11 +0900

    :sparkles: 검색된 어휘 페이지, 어휘 추가 페이지를 위한 API 추가

--- src/api/voca.ts
+++ src/api/voca.ts
@@ -1,12 +1,66 @@
 import { Router } from 'express';
 import tokenHandlers from '@middlewares/tokenHandlers';
-import { findTotalVocaCount } from '@services/voca';
+import {
+  findTotalVocaCount,
+  findVoca,
+  findVocaAntonyms,
+  findVocaCognates,
+  findVocaList,
+  findVocaMeanings,
+  findVocaSynonyms
+} from '@services/voca';
 
 const router = Router();
+
+router.get('/', async (req, res) => {
+  const {
+    query: { lang, headword, order }
+  } = req;
+  const voca = await findVoca(
+    lang as string,
+    headword as string,
+    order as string
+  );
+
+  if (!voca?.length) {
+    return res.end();
+  }
+
+  const { voca_id, etymology } = voca[0];
+  const is_unique = voca.length === 1;
+  const [meanings, synonyms, antonyms, cognates] = await Promise.all([
+    findVocaMeanings(voca_id),
+    (await findVocaSynonyms(voca_id))?.[0].synonyms ?? null,
+    (await findVocaAntonyms(voca_id))?.[0].antonyms ?? null,
+    (await findVocaCognates(voca_id))?.[0].cognates ?? null
+  ]);
+
+  res.json({
+    voca_id,
+    is_unique,
+    meanings,
+    synonyms,
+    antonyms,
+    cognates,
+    etymology
+  });
+});
 
 router.get('/count', async (req, res) => {
   const count = await findTotalVocaCount();
   res.json(count);
+});
+
+router.get('/list', async (req, res) => {
+  const {
+    query: { keyword, lang, excluded_lang }
+  } = req;
+  const vocaList = await findVocaList({
+    keyword: keyword as string,
+    langName: lang as string,
+    excludedLangName: excluded_lang as string
+  });
+  res.json(vocaList);
 });
 
 export default router;
--- src/services/voca.ts
+++ src/services/voca.ts
@@ -8,3 +8,176 @@ export const findTotalVocaCount = async () => {
   `);
   return count;
 };
+
+export const findVoca = async (
+  langName: string,
+  headword: string,
+  vocaOrder: string
+) => {
+  const voca = await DB.query<Model.Voca[]>(`
+    SELECT *
+    FROM voca V
+    WHERE
+      lang_name = '${langName}'
+      AND headword = '${headword}'
+      AND (
+        voca_order = '${vocaOrder}' OR
+        voca_order = '${Number(vocaOrder) + 1}'
+      );
+  `);
+  return voca;
+};
+
+export const findVocaMeanings = async (vocaId: number) => {
+  const meanings = await DB.query<Model.MeaningList[]>(`
+    SELECT
+      pos_name,
+      JSON_ARRAYAGG(meanings) AS meanings
+    FROM (
+      SELECT
+      voca_id,
+      pos_id,
+        JSON_OBJECT(
+          'meaning_id', M.meaning_id,
+          'meaning_text', meaning_text,
+          'examples', IF (example_id IS NULL, NULL, JSON_ARRAYAGG(
+            JSON_OBJECT (
+              'example_text', example_text,
+              'example_translation', example_translation
+            )
+          ))
+        ) AS meanings
+      FROM meaning M
+      LEFT JOIN (
+        SELECT *
+        FROM example
+        ORDER BY example_id
+        LIMIT 18446744073709551615
+      ) E
+      ON M.meaning_id = E.meaning_id
+      WHERE voca_id = '${vocaId}'
+      GROUP BY M.meaning_id
+      ORDER BY example_order
+    ) M
+    LEFT JOIN pos P
+    ON P.pos_id = M.pos_id
+    GROUP BY P.pos_id
+  `);
+  return meanings;
+};
+
+export const findVocaCognates = async (vocaId: number) => {
+  const cognates = await DB.query<{ cognates: Model.Voca[] }[]>(`
+    SELECT
+      IF (voca1_id IS NULL OR voca2_id IS NULL, NULL, JSON_ARRAYAGG(
+			  JSON_OBJECT(
+				  'voca_id', IF (voca1_id = V.voca_id, voca2_id, voca1_id),
+          'lang_name', IF (voca1_id = V.voca_id, voca2_lang_name, voca1_lang_name),
+          'headword', IF (voca1_id = V.voca_id, voca2_headword, voca1_headword),
+          'voca_order', IF (voca1_id = V.voca_id, voca2_voca_order, voca1_voca_order)
+        )
+		  )) AS cognates
+    FROM voca V
+    LEFT JOIN (
+		  SELECT
+		  	C.*,
+        V1.lang_name AS voca1_lang_name,
+        V1.headword AS voca1_headword,
+        V1.voca_order AS voca1_voca_order,
+        V2.lang_name AS voca2_lang_name,
+        V2.headword AS voca2_headword,
+        V2.voca_order AS voca2_voca_order
+      FROM cognate C
+      JOIN voca V1
+      ON C.voca1_id = V1.voca_id
+      JOIN voca V2
+      ON C.voca2_id = V2.voca_id
+    ) C
+    ON V.voca_id = C.voca1_id OR V.voca_id = C.voca2_id
+    WHERE V.voca_id = '${vocaId}'
+    GROUP BY V.voca_id
+  `);
+  return cognates;
+};
+
+export const findVocaSynonyms = async (vocaId: number) => {
+  const synonyms = await DB.query<{ synonyms: Model.Voca[] }[]>(`
+    SELECT
+      IF (voca1_id IS NULL OR voca2_id IS NULL, NULL, JSON_ARRAYAGG(
+			  JSON_OBJECT(
+				  'voca_id', IF (voca1_id = V.voca_id, voca2_id, voca1_id),
+          'headword', IF (voca1_id = V.voca_id, voca2_headword, voca1_headword),
+          'voca_order', IF (voca1_id = V.voca_id, voca2_voca_order, voca1_voca_order)
+        )
+		  )) AS synonyms
+    FROM voca V
+    LEFT JOIN (
+		  SELECT
+		  	S.*,
+        V1.headword AS voca1_headword,
+        V1.voca_order AS voca1_voca_order,
+        V2.headword AS voca2_headword,
+        V2.voca_order AS voca2_voca_order
+      FROM synonym S
+      JOIN voca V1
+      ON S.voca1_id = V1.voca_id
+      JOIN voca V2
+      ON S.voca2_id = V2.voca_id
+    ) S
+    ON V.voca_id = S.voca1_id OR V.voca_id = S.voca2_id
+    WHERE V.voca_id = '${vocaId}'
+    GROUP BY V.voca_id
+  `);
+  return synonyms;
+};
+
+export const findVocaAntonyms = async (vocaId: number) => {
+  const antonyms = await DB.query<{ antonyms: Model.Voca[] }[]>(`
+    SELECT
+      IF (voca1_id IS NULL OR voca2_id IS NULL, NULL, JSON_ARRAYAGG(
+			  JSON_OBJECT(
+				  'voca_id', IF (voca1_id = V.voca_id, voca2_id, voca1_id),
+          'headword', IF (voca1_id = V.voca_id, voca2_headword, voca1_headword),
+          'voca_order', IF (voca1_id = V.voca_id, voca2_voca_order, voca1_voca_order)
+        )
+		  )) AS antonyms
+    FROM voca V
+    LEFT JOIN (
+		  SELECT
+		  	A.*,
+        V1.headword AS voca1_headword,
+        V1.voca_order AS voca1_voca_order,
+        V2.headword AS voca2_headword,
+        V2.voca_order AS voca2_voca_order
+      FROM antonym A
+      JOIN voca V1
+      ON A.voca1_id = V1.voca_id
+      JOIN voca V2
+      ON A.voca2_id = V2.voca_id
+    ) A
+    ON V.voca_id = A.voca1_id OR V.voca_id = A.voca2_id
+    WHERE V.voca_id = '${vocaId}'
+    GROUP BY V.voca_id
+  `);
+  return antonyms;
+};
+
+export const findVocaList = async ({
+  keyword,
+  langName,
+  excludedLangName
+}: {
+  keyword: string;
+  langName?: string;
+  excludedLangName?: string;
+}) => {
+  const vocaList = await DB.query<Model.Voca[]>(`
+    SELECT * FROM voca
+    WHERE headword LIKE '${`${keyword.replaceAll("'", "''")}%`}'
+    ${langName ? `AND lang_name = '${langName}'` : ''}
+    ${excludedLangName ? `AND lang_name != '${excludedLangName}'` : ''}
+    ORDER BY headword
+    LIMIT 10;
+  `);
+  return vocaList;
+};
